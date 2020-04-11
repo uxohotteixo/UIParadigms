a2cc7415f2a79e025149baa1ccb17eee0ce16acb
Moskvinr <ilnurkutlubaev7@gmail.com>
2020-04-11 12:38:06 +0300

    frontend

---
+++ src/UIParadigms.Apps.Web/Constants/CommandConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace UIParadigms.Apps.Web.Constants
+{
+    public static class CommandConstants
+    {
+        public const string Show = "show";
+        public const string Move = "move";
+        public const string Create = "create";
+        
+    }
+}
---
+++ src/UIParadigms.Apps.Web/Controllers/CommandsController.cs
@@ -0,0 +1,52 @@
+﻿using System.IO;
+using Microsoft.AspNetCore.Mvc;
+using UIParadigms.Modules.Interpretator;
+
+namespace UIParadigms.Apps.Web.Controllers
+{
+    [Route("[controller]/[action]")]
+    public class CommandsController : ControllerBase
+    {
+        private readonly CommandInterpreter _interpreter;
+
+        public CommandsController(CommandInterpreter interpreter)
+        {
+            _interpreter = interpreter;
+        }
+
+        [HttpGet]
+        public IActionResult Show(string parameters = "")
+        {
+            return Ok(_interpreter.Interpret(parameters));
+        }
+
+        [HttpGet]
+        public IActionResult Open(string parameters)
+        {
+            return Ok(_interpreter.Interpret(parameters));
+        }
+
+        [HttpPost]
+        public IActionResult Move([FromBody]InputParams parameters)
+        {
+            return Ok(_interpreter.Interpret(parameters.Parameters));
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] InputParams parameters)
+        {
+            return Ok(_interpreter.Interpret(parameters.Parameters));
+        }
+
+        [HttpPost]
+        public IActionResult Remove([FromBody]InputParams parameters)
+        {
+            return Ok(_interpreter.Interpret(parameters.Parameters));
+        }
+    }
+
+    public class InputParams
+    {
+        public string Parameters { get; set; }
+    }
+}
